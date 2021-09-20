51aa2d94ab8ae8dc34e2c06c20f1ba7a77f7d9e8
Phu-bit <57963725+Phu-bit@users.noreply.github.com>
2021-09-20 13:38:40 -0600

    hello world

---
+++ Assets/Week 2 Lines/SinStuff.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinStuff : MonoBehaviour
+{
+    public Vector3 frequency, amplitude, offset;
+    public bool move, rotate, size;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Mathf.Sin(frequency.x * Time.time + offset.x) * amplitude.x;
+        float y = Mathf.Sin(frequency.x * Time.time + offset.y) * amplitude.y;
+        float z = Mathf.Sin(frequency.x * Time.time + offset.z) * amplitude.z;
+
+        Vector3 sinVector = new Vector3(x * x / amplitude.x, y, z);
+
+        if (move)
+        {
+            this.transform.localPosition = sinVector;
+        }
+        if (rotate)
+            this.transform.localEulerAngles = sinVector;
+        if (size)
+            this.transform.localScale = sinVector;
+    }
+}
---
+++ Assets/Week 2 Lines/moving.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour
+{
+
+    public bool worldSpace, isMoving, useMethods, localAxis, useSin;
+    public Vector3 position, rotation, scale, move, rotate, size;
+    public Vector3 frequency, amplitude, offeset;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isMoving)
+        {
+            if (worldSpace)
+            {
+                this.transform.position = position;
+                this.transform.eulerAngles = rotation;
+
+            }
+            else //localspace
+            {
+                this.transform.localPosition = position;
+                this.transform.localEulerAngles = rotation;
+                this.transform.localScale = scale;
+
+            }
+
+        }
+        else
+        {
+            if (useMethods)
+            {
+                if (localAxis)
+                {
+                    this.transform.Translate(move);
+                    this.transform.Rotate(rotate);
+
+                }
+                else
+                {
+                    this.transform.Translate(this.transform.forward * move.z);
+                    this.transform.Rotate(rotate);
+                }
+            }
+            else
+            {
+                float x = Mathf.Sin(frequency.x * Time.deltaTime ) * amplitude.x;
+                float y = Mathf.Sin(frequency.x * Time.deltaTime ) * amplitude.y;
+                float z = Mathf.Sin(frequency.x * Time.deltaTime ) * amplitude.z;
+
+                Vector3 sinVector = new Vector3(x, y, z);
+                this.transform.localPosition += move;
+                this.transform.localEulerAngles += rotate;
+                this.transform.localScale += size;
+            }
+        }
+
+    }
+}
